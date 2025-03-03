b513b866aeee1a71420692f4445f45f317f54893
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2025-03-02 17:30:20 -0800

    Working PCf control

--- src/stopwatch-pcf/README.md
+++ src/stopwatch-pcf/README.md
@@ -8,7 +8,7 @@ This sample was created using the following command
 pac pcf init -n Stopwatch -ns WebAssembly -t field -npm
 ```
 
-## Prerequistes
+## Prerequisites
 
 1. You have Power Platform Command Line Interface [installed](https://learn.microsoft.com/power-platform/developer/cli/introduction?tabs=windows). 
 
@@ -34,7 +34,58 @@ npm install
 npm run start
 ```
 
-## Web Resources
+## Package the solution
+
+To package a version of the PCF control
+
+1. Build the control
+
+```pwsh
+npm run build
+```
+
+2. Change to solution folder
+
+```pwsh
+cd stopwatch_pcf_control
+```
+
+3. Build the solution
+
+```pwsh
+cd stopwatch_pcf_control
+```
+
+4. Upload the create **stopwatch_pcf_control.zip** to your environment
+
+## Power App Sample
+
+The [DotNetWebAssembly_1_0_0_1.zip](./DotNetWebAssembly_1_0_0_1.zip) include a sample of using this PCF control inside a Power Platform solution. To use this sample
+
+1. Import the solution to an environment
+
+2. Publish all customizations
+
+3. Run the **Web Assembly Manager** Model-Driven App
+
+4. Create Dataverse records for the results of the **publish** folder of the [Stopwatch](../stopwatch/README.md) sample
+
+  ![Sample Admin Screen with Stopwatch Web Assembly components loaded](./media/WebAssemblyAdmin.png)
+
+5. Run the loaded application
+
+  ![Running application](./media/WebAssembly-StopwatchSample.png)
+
+## Options
+
+The following options were evaluated to host the JavaScript and DotNet Web Assembly components
+
+- Web Resources
+- Dataverse Web Api
+
+Datavesre Web Api was selected for this sample as it allows the ability to update the Web Assembly component at run time. It also had a more granular security model that can be applied using Security Roles.
+
+### Web Resources
 
 Web resources in Power Apps are virtual files stored in the Microsoft Dataverse database. They can be used to extend the functionality of model-driven apps or PCF controls by adding custom HTML, JavaScript, CSS, and other file types. Here are some key points to consider when working with web resources:
 
@@ -45,3 +96,21 @@ Web resources in Power Apps are virtual files stored in the Microsoft Dataverse 
 2. [**Supported File Types**](https://learn.microsoft.com/power-apps/developer/model-driven-apps/web-resources##web-resource-types): Power Apps web resources support a variety of file types, including HTML, JavaScript, CSS, XML, and image formats like PNG, JPG, and GIF. However, certain file types such as `.wasm`, `.wasm.gz`, `.dat`, `.wasm.br`, and `.dat.br` are not supported. Given dotnetwasm uses these file types, we will consider using alternative extensions or methods to include them in the project.
 
 3. **Execution Context**: Web resources are limited to static files or files processed in the browser. They do not support server-side execution like ASP.NET (.aspx) pages. This means that any code within a web resource must be executed on the client side.
+
+### Dataverse Web Api
+
+The Dataverse Web API is a RESTful web service interface that allows you to interact with data stored in Microsoft Dataverse. Dataverse is a cloud-based storage solution that is part of the Microsoft Power Platform, which includes Power Apps, Power Automate, Power BI, and Power Virtual Agents. The Web API uses the OData (Open Data Protocol) version 4.0 standard, which is a protocol for building and consuming RESTful APIs over rich data sources.
+
+With the Dataverse Web API, you can perform various operations such as creating, retrieving, updating, and deleting data. These operations can be performed both synchronously and asynchronously, making the API suitable for a wide range of applications. The Web API also supports querying data using OData query options, allowing you to filter, sort, and paginate results.
+
+One of the key advantages of using the Dataverse Web API is its granular security model. The API respects the security roles and permissions defined in Dataverse, ensuring that only authorized users can access and modify data. This security model is applied using Security Roles, which can be configured to grant or restrict access to specific entities and operations.
+
+In addition to the basic CRUD (Create, Read, Update, Delete) operations, the Dataverse Web API also supports more advanced features such as batch operations, metadata operations, and executing custom actions and functions. This makes it a powerful tool for building complex applications that require interaction with Dataverse data.
+
+#### Extension Model with Model-Driven Applications
+
+Model-driven applications in Power Apps provide a way to build sophisticated business applications with minimal code. These applications are built on top of Dataverse and leverage its data model, security, and business logic. One of the key features of model-driven applications is the ability to extend their functionality using the Power Apps Component Framework (PCF).
+
+The Power Apps Component Framework allows developers to create custom components that can be embedded in model-driven applications. These components can interact with the Dataverse Web API to query and manipulate data. When a custom component makes a request to the Web API, it uses the security context of the logged-in user. This means that the component can only access data that the user has permission to view or modify, ensuring that the security model is maintained.
+
+By using the Dataverse Web API and the Power Apps Component Framework, developers can create rich, interactive components that enhance the functionality of model-driven applications while respecting the security and data integrity of Dataverse. This combination provides a powerful platform for building enterprise-grade applications that can be customized to meet the specific needs of your organization.
--- src/stopwatch-pcf/Stopwatch/index.ts
+++ src/stopwatch-pcf/Stopwatch/index.ts
@@ -1,9 +1,13 @@
+import { Console } from "console";
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 
 export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
     private container: HTMLDivElement;
     private _context: ComponentFramework.Context<IInputs>
+    private baseUrl: string;
+     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private originalFetch: any;
 
     /**
      * Empty constructor.
@@ -27,25 +31,195 @@ export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IO
         this._context = context;
         this.loadHtmlContent();
 
-        // Query the record with name 'dotnet.js.gz' in the 'dev_name' field
-        const query = "?$filter=dev_name eq 'dotnet.js.gz'";
-        this._context.webAPI.retrieveMultipleRecords("dev_webassembly", query)
-            .then((result) => {
-                if (result.entities.length > 0) {
-                    // Record found, proceed to retrieve the dev_content column
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    const id = result.entities[0]["dev_webassemblyid"];
-                    this.fetchFileContent(id);
-                } else {
-                    console.log("No record found with the specified name.");
-                }
-            })
-            .catch((error) => {
-                console.error(error.message);
-            });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const windowInstance = window as any;
+
+        // Dynamically set the base URL so that can handle relative paths for .Net WASM files
+        this.baseUrl = windowInstance.location.origin + "/_framework/";
+
+        this.overrideFetch();
+        //this.overrideXMLHttpRequest();
+
+        const baseElement = document.createElement("base");
+        baseElement.href = this.baseUrl;
+        document.head.appendChild(baseElement);
+
+        // const boot = await this.getFileIdentifer('blazor.boot.json.gz');
+        // const bootContent = await this.fetchFileFromWebApi(boot);
+        // const decompressedBootContent = await this.decompressFileContentText(bootContent);
+
+        // Query the record with name 'dotnet.js.gz' to boostrap the .Net WASM load process
+        // This process will then call blazor.boot.json to request manifest of files to download
+
+        // TODO: Configuring the app to fetch and decode Brotli compressed files *.br not gz)
+        const id = await this.getFileIdentifer('dotnet.js.gz');
+        this.fetchDotNetJsContent(id);
     }
 
-    private fetchFileContent(fileIdentifier: string): void {
+    /**
+     * Overrides the fetch method for _framework files so that can load files from the WebApi rather than static files
+     */
+    private overrideFetch() : void {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const globalThisInstance = globalThis as any;
+        if (typeof this.originalFetch === 'undefined') {
+            this.originalFetch = globalThisInstance.fetch;
+        }
+        globalThisInstance.fetch = async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+            let url;
+            if (typeof input === 'string') {
+                url = input;
+            } else if (input instanceof Request) {
+                url = input.url;
+            }
+            console.log("Fetch URL", url);
+
+            // Check if load file from the root path
+            const rootFile = url?.replace(this.baseUrl.replace('_framework/',''),'').indexOf('/') == -1;
+        
+            if (url && (url.startsWith(this.baseUrl) || rootFile)) {
+                // Fetch the file from WebApi with .gz extension
+                url = url.replace(this.baseUrl, '');
+                url = url.replace(this.baseUrl.replace('_framework/',''), '');
+
+                if (!url.endsWith('.gz')) {
+                    url += '.gz';
+                }
+                const fileId = await this.getFileIdentifer(url);
+                const fileContent = await this.fetchFileFromWebApi(fileId);
+                let mimeType = url.endsWith('.json.gz') ? 'application/json' : 'application/javascript';
+                if (url.endsWith('.js.gz')) {
+                    mimeType = 'application/javascript';
+                }
+                if (url.endsWith('.wasm.gz')) {
+                    mimeType = 'application/wasm';
+                }
+                if (url.endsWith('.dat.gz')) {
+                    mimeType = 'application/dat';
+                }
+
+                let response: Response;
+             
+                if (mimeType == 'application/javascript') {
+                    let decompressedFileContent = await this.decompressFileContentText(fileContent);
+                    decompressedFileContent = this.PCFEnableDotNetJs(decompressedFileContent);
+                    response = new Response(decompressedFileContent, { headers: { 'Content-Type': mimeType } });
+                } else {
+                    const decompressedFileContent = await this.decompressFileContentArray(fileContent);
+                    response = new Response(decompressedFileContent, { headers: { 'Content-Type': mimeType } });
+                }
+
+                return response;
+            }
+        
+            return this.originalFetch(input, init);
+        };
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const windowInstance = window as any;
+        windowInstance.fetch = globalThisInstance.fetch;
+    }
+
+    /**
+     * Gets the file identifier for the specified file name using a lookup in the WebApi
+     * @param fileName The name of the file to get the identifier for
+     * @returns The unique identifier of the file in Dataverse WebApi
+     */
+    private async getFileIdentifer(fileName: string): Promise<string> {
+        if (!fileName.endsWith('.gz')) {
+            fileName += '.gz';
+        }
+        // Query the record with name 'dotnet.js.gz' in the 'dev_name' field
+        const query = `?$filter=dev_name eq '${fileName}'`;
+        try {
+            const result = await this._context.webAPI.retrieveMultipleRecords("dev_webassembly", query);
+            if (result.entities.length > 0) {
+                // Record found, proceed to retrieve the dev_content column
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                return result.entities[0]["dev_webassemblyid"];
+            } else {
+                throw new Error("No record found with the specified name.");
+            }
+         // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            console.error(error.message);
+            throw error;
+        }
+    }
+
+    /**
+     * Fetches the file content from the WebApi
+     * @param fileId The unique identifier of the record to retreieve the file content for
+     * @returns 
+     */
+    private async fetchFileFromWebApi(fileId: string): Promise<string> {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const context: any = this._context;
+        const baseUrl = context.page.getClientUrl();
+        const url = `${baseUrl}/api/data/v9.2/dev_webassemblies(${fileId})/dev_content`;
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json;odata=verbose'
+            }
+        });
+        const data = await response.json();
+        return data.value;
+    }
+
+    /**
+     * Decompresses the file content from a gzip file content and loads it into the application as text
+     * @param fileContent The file content as a base64 encoded string.
+     * @returns The uncompressed file content as a string
+     */
+    private async decompressFileContentText(fileContent: string): Promise<string> {
+        const binaryString = atob(fileContent);
+        const len = binaryString.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
+        const blob = new Blob([bytes], { type: 'application/gzip' });
+        const ds = new DecompressionStream('gzip');
+        const decompressedStream = blob.stream().pipeThrough(ds);
+        const decompressedBlob = await new Response(decompressedStream).blob();
+
+        // Convert the decompressed content to text
+        return await decompressedBlob.text();
+    }
+
+    /**
+     * Decompresses the file content from a gzip file content and loads it into the application as binary array
+     * @param fileContent The file content as a base64 encoded string.
+     * @returns The uncompressed file content as a binary array
+     */
+    private async decompressFileContentArray(fileContent: string): Promise<Uint8Array> {
+        const binaryString = atob(fileContent);
+        const len = binaryString.length;
+        const bytes = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            bytes[i] = binaryString.charCodeAt(i);
+        }
+        const blob = new Blob([bytes], { type: 'application/gzip' });
+        const ds = new DecompressionStream('gzip');
+        const decompressedStream = blob.stream().pipeThrough(ds);
+        const decompressedBlob = await new Response(decompressedStream).blob();
+
+        // Convert the decompressed content to array
+        return await this.blobToByteArray(decompressedBlob);
+    }
+
+    private async blobToByteArray(blob: Blob) : Promise<Uint8Array> {
+        return blob.arrayBuffer().then(function(arrayBuffer) {
+            return new Uint8Array(arrayBuffer);
+        });
+    }
+
+    /**
+     * Fetches the dotnet.js file content from the WebApi.
+     * @param fileIdentifier The unique identifier of the record to retreieve the file content for
+     */
+    private fetchDotNetJsContent(fileIdentifier: string): void {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const context: any = this._context;
         const baseUrl = context.page.getClientUrl();
@@ -59,14 +233,18 @@ export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IO
         .then(response => response.json())
         .then(data => {
             const fileContent = data.value;
-            this.decompressFileContent(fileContent);
+            this.decompressDotNetJsFileContent(fileContent);
         })
         .catch(error => {
             console.error(error.message);
         });
     }
 
-    private async decompressFileContent(fileContent: string): Promise<void> {
+    /**
+     *  Decompresses the dotnet.js from a gzip file content and loads it into the application.
+     * @param fileContent The file content as a base64 encoded string.
+     */
+    private async decompressDotNetJsFileContent(fileContent: string): Promise<void> {
         const binaryString = atob(fileContent);
         const len = binaryString.length;
         const bytes = new Uint8Array(len);
@@ -79,26 +257,85 @@ export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IO
         const decompressedBlob = await new Response(decompressedStream).blob();
 
         // Convert the decompressed content to text
-        const decompressedTextContent = await decompressedBlob.text();
+        const decompressedTextContent = this.PCFEnableDotNetJs(await decompressedBlob.text());
        
         // Create a JSON Blob with the correct MIME type
         const jsonBlob = new Blob([decompressedTextContent], { type: 'application/javascript' });  
-
+        
         // Create a URL for the JSON Blob and pass it to loadDotnetJs
         await this.loadDotnetJs(URL.createObjectURL(jsonBlob));
     }
 
+    /**
+     * Modifies the dotnet.js and related JavaScript content to enable it to work in PCF.
+     * @param dotnetJsContent The content of the dotnet.js file
+     * @returns The modified content of the dotnet.js file
+     */
+    private PCFEnableDotNetJs(dotnetJsContent: string): string {
+        // **** IMPORTANT *****
+        // This code is work in progress PREVIEW code that looks to apply changes to dotnet.js and related WASM runtime code to enable them to work in PCF
+        // The code and dotnet.js code is subject to change at any time
+        
+        // ASSUMPTIONS:
+        // 1. globalThis.fetch and window.fetch have been overridden to use the custom fetch implementation
+        // 2. The custom fetch implementation will load the .gz files from the WebApi
+        // 3. Requests not related to baseUrl will be passed to the original fetch implementation
+        // 4. JavaScript librarues are loaded as blob URLs and will redirect to baseUrl + "/framework/" path
+        // 5. Only supported in Model Driven Application or Custom Page of MDA as requires calls to WebApi
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const globalThisInstance = globalThis as any;
+
+        globalThisInstance.getDocument = () => { return document; };
+
+        // Refer to the PCF control not the module
+        dotnetJsContent = dotnetJsContent.replaceAll("globalThis.document", `globalThis.getDocument()`);
+
+        // Replace script directory to remove the blob: prefix
+        dotnetJsContent = dotnetJsContent.replaceAll("qe.locateFile=", "qe.scriptDirectory=qe.scriptDirectory.replace('blob:',''),qe.scriptDirectory+='_framework/',qe.locateFile=");
+
+        // Lets fix custom import js using blob
+
+        // ... Make import custom import using WebApi rather than direct request
+        dotnetJsContent = dotnetJsContent.replaceAll("=import(", "=globalThis.customImport(");
+        dotnetJsContent = dotnetJsContent.replaceAll("await import(", "await globalThis.customImport(");
+
+        // Fix the URL for dotnet.native.wasm to load relatd to import.meta.url
+        dotnetJsContent = dotnetJsContent.replaceAll('new URL("dotnet.native.wasm",import.meta.url)', `new URL("dotnet.native.wasm",'https://'+globalThis.getDocument().location.host+'/_framework/')`);
+        dotnetJsContent = dotnetJsContent.replaceAll('_scriptDir = import.meta.url', '_scriptDir = "https://"+globalThis.getDocument().location.host+"/_framework/"');
+
+        // Replace readAsync with globalThis.fetch
+        dotnetJsContent = dotnetJsContent.replaceAll('DOTNET.setup', 'readAsync = (filename, onload, onerror, binary = true) => globalThis.fetch(new URL(filename)).then(response => response.ok ? (binary ? response.arrayBuffer() : response.text()).then(onload) : Promise.reject(response.statusText)).catch(onerror);DOTNET.setup');
+
+        globalThisInstance.customImport = async (url: string) => {
+            const response = await globalThisInstance.fetch(url);
+            let text = await response.text();
+            text = text.replaceAll('await import(', 'await globalThis.customImport(');
+            const jsonBlob = new Blob([text], { type: 'application/javascript' });  
+            return import(/* webpackIgnore: true */ URL.createObjectURL(jsonBlob));
+        }
+
+        return dotnetJsContent;
+    }
+
+    /**
+     * Loads the HTML content into the container. The #time div will be updated with the stopwatch time.
+     */
     private loadHtmlContent(): void {
         const htmlContent = `
             <div id="control-container">
                 <button id="reset" style="color:red">Reset</button>
                 <button id="pause" style="color:green">Pause</button>
-                <div id="output"></div>
+                <div id="time"></div>
             </div>
         `;
         this.container.innerHTML = htmlContent;
     }
 
+    /**
+     * Async loads the dotnet.js file and initializes the DotNet Web Assembly application.
+     * @param blobUrl The Blob URL of the dotnet.js file.
+     */
     private async loadDotnetJs(blobUrl: string): Promise<void> {    
         try {
             const module = await import(/* webpackIgnore: true */ blobUrl);
@@ -108,15 +345,38 @@ export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IO
                 console.error("Dotnet instance is undefined");
                 return;
             }
+
+            // Override the fetch method to use the custom fetch implementation
+            this.overrideFetch();
         
-            const { getAssemblyExports, getConfig, runMain } = module.dotnet
+            const { setModuleImports, getAssemblyExports, getConfig, runMain } = await module.dotnet
+                .withConfigSrc(this.baseUrl + "blazor.boot.json")
                 .withApplicationArguments("start")
                 .create();
+
+            // Define the type for the dom object
+            interface Dom {
+                setInnerText: (selector: string, time: string) => void;
+            }
+
+            // Define callback for the dom object from .Net to update the time
+            setModuleImports('main.js', {
+                dom: {
+                    setInnerText: (selector : string, time: string) => {
+                        const element = document.querySelector(selector);
+                        if (element) {
+                            (element as HTMLElement).innerText = time;
+                        }
+                    }
+                }
+            });
         
             console.log("Dotnet script initialized");
             const config = getConfig();
         
             console.log("Dotnet script config", config);
+
+            // Get the assembly exports to Reset and Toggle the stopwatch
             const exports = await getAssemblyExports(config.mainAssemblyName);
         
             document.getElementById('reset')!.addEventListener('click', e => {
@@ -134,6 +394,7 @@ export class Stopwatch implements ComponentFramework.StandardControl<IInputs, IO
                 e.preventDefault();
             });
         
+            // Run the main function to start the application in the UI thread
             await runMain();
         } catch (error) {
             console.error("Error loading dotnet.js", error);
